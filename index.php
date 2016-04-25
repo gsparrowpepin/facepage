e617b19dd407b3aa1acc83922b1f6ed18f3c9497
Guillaume S-P <gsparrowpepin@gmail.com>
2016-04-25 09:44:40 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Title</title>
+		<meta name="description" content="GSP">
+		<meta name="author" content="Guillaume Sparrow-Pepin">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+	<h1 class="header" id="logo">Hello Mary</h1>
+	<h1 class="header">Subtitle</h1>
+	<p>This is soo cool</p>
+	</body>
+</html>
