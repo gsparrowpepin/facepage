a99c9f34b59467f16526dac18be0a01734413445
Guillaume S-P <gsparrowpepin@gmail.com>
2016-04-26 13:21:45 -0400

    script now working added lightbox

--- 9th.php
+++ 9th.php
@@ -1,20 +1,55 @@
-<!doctype html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>Facepage</title>
-		<meta name="description" content="GSP">
-		<meta name="author" content="Guillaume Sparrow-Pepin">
-		<link rel="stylesheet" href="style.css">
-	</head>
+<?php include('top.php') ?>
 	<body>
 		<?php
-			$dir    = '/9th';
-			$files1 = scandir($dir);
-			$files2 = scandir($dir, 1);
+			$dir    = '9th';
+			$files = scandir($dir);
 
-			print_r($files1);
-			print_r($files2);
+			unset($files[0]);
+			unset($files[1]);
+
+			$studentNames = array();
+
+			foreach ($files as $file) {
+				$fileName = preg_replace('/\\.[^.\\s]{3,4}$/', '', $file);
+
+				//Add First Character
+				$pieces = explode("'", $fileName);
+				$imploded = $pieces[0];
+
+				for ($j = 1; $j < count($pieces); $j++) {
+					if ($j % 2 == 0) {
+						#second quote
+						$imploded .= "' " . $pieces[$j];
+					} else {
+						#first quote
+						$imploded .= " '" . $pieces[$j];
+					}
+				}
+
+				$buildStr = substr($imploded, 0, 1);
+
+				for( $i = 1; $i < strlen($imploded); $i++ ) {
+
+						$prevChar = substr( $imploded, $i - 1, 1 );
+					    $char = substr( $imploded, $i, 1 );
+					    $nextChar = substr( $imploded, $i + 1, 1 );
+						
+						if (ctype_upper($char) && $prevChar != "-" && $prevChar != "'" && $prevChar != " ") {
+					    	//Char is uppercase, add space before
+					    	$buildStr .= " " . $char;
+					    } else {
+					    	$buildStr .= $char;
+					    }
+					}
+
+				?>
+					<div class="studentProfile gallery">
+						<img class="studentImage" src="<?php echo '9th/' . $file;?>">
+						<p class="studentName"><?php echo $buildStr;?></p>
+						
+					</div>
+				<?php
+			} // end for each file
 		?>
 	</body>
 </html>
